59a3363a224c832ac9a318aef0ed9ca9a658e976
DwarfPorter <mrvlmor@gmail.com>
2021-06-09 20:31:21 +0300

    первый коммит

---
+++ Lesson-5/Accoun/app/src/main/java/ru/geekbrains/accoun/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.accoun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
